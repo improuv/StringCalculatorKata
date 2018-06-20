7e50ce9b2f6efd256b66569db144fba73f359f0b
seb <sebastian.heglmeier@improuv.com>
2018-06-20 12:18:49 +0200

    local intellij markdown interpreter seems to be more generous than the one on github.com ;-)

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,8 @@
+public class StringCalculator {
+
+    public int add(String number) {
+        if(number.isEmpty())
+            return 0;
+        return Integer.parseInt(number);
+    }
+}
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,16 +1,40 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+/**
+ * github.com/improuv/StringCalculatorKata
+ */
 public class StringCalculatorTest {
 
+    private StringCalculator calculator;
+
+    @Ignore
     @Test
     public void dummyTest1() {
-        assertThat("reality", is("expected value"));
+        assertThat("2", is("2"));
     }
 
+    @Ignore
     @Test
     public void dummyTest2() {
         assertThat(true, is(not(false)));
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void emptyString_shouldReturn0() {
+        assertThat(calculator.add(""), is(0));
+    }
+
+    @Test
+    public void singleNumber_shouldBeReturnedAsIs() {
+        assertThat(calculator.add("77"), is(77));
     }
 }
